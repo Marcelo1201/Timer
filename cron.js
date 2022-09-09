8c6f4e3e52b8a33c20337b12b0d11c2206960b90
Marcelo Giacometti <marcelo.giacomettigemeo1@gmail.com>
2022-09-09 17:32:38 -0300

    Finishing JavaScript and footer

---
+++ cron.js
@@ -0,0 +1,44 @@
+var sec = 00;
+var min = 00;
+var hr = 00;
+var interval;
+
+function digits(number){
+    if(number<10){
+        return('0' + number);
+    } else{
+        return(number);
+    }
+}
+
+function start(){
+    timer();
+    interval = setInterval(timer, 1000);
+    document.querySelector('.start').disabled = true;
+}
+
+function stop(){
+    clearInterval(interval);
+    document.querySelector('.start').disabled = false;
+}
+
+function reset(){
+    clearInterval(interval);
+    sec = 0;
+    min = 0;
+    document.getElementById('cron').innerHTML = '00:00:00';
+    document.querySelector('.start').disabled = false;
+}
+
+function timer(){
+    sec++;
+    if(sec == 60){
+        min++;
+        sec = 0;
+        if(min == 60){
+            hr++
+            min = 0;
+        }
+    }
+    document.getElementById('cron').innerHTML =digits(hr) + ':' + digits(min) + ':' + digits(sec);
+}
